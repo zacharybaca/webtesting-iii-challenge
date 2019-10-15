19b957bc17595ef138fe42b6aad8e0295703c86f
zacharybaca <baca.zach@icloud.com>
2019-10-15 14:49:59 -0500

    complete

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,21 @@
+import React from 'react';
+import {render, fireEvent} from 'react-testing-library';
+
+import Display from './Display';
+
 // Test away!
+
+//Gate Test
+describe('Display Component', () => {
+    it("Shows state unlocked and open", () => {
+        const { getByText } = render(<Display />);
+        expect(getByText('Unlocked'));
+        expect(getByText('Open'));
+    })
+
+    it('Changes through props', () => {
+        const { getByText } = render(<Display locked={true} closed={true} />);
+        expect(getByText("Locked"));
+        expect(getByText("Closed"));
+    })
+})
